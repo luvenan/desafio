a98371052e87d5e9795170592f037cec8ab2e1d2
luvenan <luvenan@gmail.com>
2022-02-17 17:45:35 -0300

    Corrigi código de questões 01 e 03

--- Resources/questao2.js
+++ Resources/questao2.js
@@ -1,54 +1,60 @@
-function verificaSenha(senha) {
+function validarSenha(senha) {
+    let senhaValida = true;
     //Verifica se a senha tem no minimo 6 caracteres
     if(senha.length < 6) {
         console.log(senha);
         console.log("A senha precisa ter no minimo 6 caracteres");
-        return false;
+        senhaValida = false;
     }
     //Verifica se a senha contém números
     if(!senha.match(/\d/)) {
         console.log(senha);
         console.log("A senha precisa ter no minimo 1 numero");
-        return false;
+        senhaValida = false;
     }
     //Verifica se a senha contém letras maiúsculas
     if(!senha.match(/[A-Z]/)) {
         console.log(senha);
         console.log("A senha precisa ter pelo menos uma letra maiúscula");
-        return false;
+        senhaValida = false;
     }
     //Verifica se a senha contém letras minúsculas
     if(!senha.match(/[a-z]/)) {
         console.log(senha);
         console.log("A senha precisa ter pelo menos uma letra minúscula");
-        return false;
+        senhaValida = false;
     }
     //Verifica se a senha contém caracteres especiais
     if(!senha.match(/[!@#$%^&*()-+]/)) {
         console.log(senha);
         console.log("A senha precisa ter pelo menos um caractere especial: !@#$%^&*()-+");
+        senhaValida = false;
+    }
+    if(senhaValida) {
+        console.log(senha);
+        console.log('A senha é segura!');
+        return true;
+    } else {
         return false;
     }
-    console.log(senha);
-    console.log('A senha é segura!');
-    return true;
 }
-
-//Problema: a senha pode ter mais de um erro, e eu gostaria de que ela não deixasse de conferir os outros erros se o primeiro erro for pego, mas ao mesmo tempo, eu só quero que ela imprima o resultado final se não tiver erros. Usar uma variável como boleean pra verificar?
-
 
 //Senhas testes
 
 //Senha curta
-verificaSenha("a2C@");
+validarSenha("a2C@");
 //Senha sem letra minúscula
-verificaSenha('ERQEDV3%');
+validarSenha('ERQEDV3%');
 //Senha sem letra maiúscula
-verificaSenha('qeiruqo1&');
+validarSenha('qeiruqo1&');
 //Senhas sem caracteres especiais
-verificaSenha('41u34ihQE');
+validarSenha('41u34ihQE');
 //Senhas seguras
-verificaSenha('QEereq1&');
-verificaSenha('abcd12EF*');
-verificaSenha('senhA2$%');
-verificaSenha('senhA2+(0)');
+validarSenha('QEereq1&');
+validarSenha('abcd12EF*');
+validarSenha('senhA2$%');
+validarSenha('senhA2+(0)');
+
+//senhas com vários erros
+validarSenha('qoru3');
+validarSenha('ek');
--- Resources/questao3.js
+++ Resources/questao3.js
@@ -1,5 +1,11 @@
-function criarArrSubstrings(palavra) {
+//Passos:
+//1. Criar array com todas as substrings
+//2. Crirar função que confere se uma string e anagrama de outra
+//3. Usar um nested loop pra iterar cada string do array e ver se é anagrama de outra, usando a função acima como callback e o array como argumento. Cada vez que for, a contagem de anagram pairs aumenta em um. Imprimir a contagem de anagram pairs. 
+
+
 //Encontra todas as substrings de uma string e coloca em um array
+function encontrarSubstrings(palavra) {
     let arrSubstrings = [];
     for (let i = 0; i < palavra.length; i++) {
         for (let j = i + 1; j < palavra.length + 1; j++) {
@@ -9,84 +15,47 @@ function criarArrSubstrings(palavra) {
     return arrSubstrings;
 }
 
-const test1 = criarArrSubstrings('baba');
-console.log(test1);
+// const test1 = encontrarSubstrings('baba');
+// console.log(test1);
 
-// Usa um map para modificar as substrings e fazer ficarem em ordem alfabética
-const map1 = test1.map(a => a.split('').sort().join(''));
-console.log(map1);
-
-//Possívels solução:
-//1. Criar array com todas as substrings (done)
-//2. Usar um map pra fazer as substrings ficarem em ordem alfabética
-//3. Usar um reduce pra ver se as substrings são iguais
-//4. Usar um filter pra filtrar as substrings que são iguais
-//5. Contar o número de substrings ainda no array, dividir por 2, terei o número final de anagramas
-
-
-//Outra tentativa:
-//Iterate over each array element as an argument for a function that will check how many times each string appears in the array. Ok, let's try this first;
-
-//Not working, only giving me the first time it appears it seems, plus missing longer strings... might work for counting but will come out wrong at one point, specially since it uses "indexOf"
-//  count = map1.filter((x, y) => map1.indexOf(x) !== y);
-//  console.log(count);
-
-
-//This definetly works, counts the number of string pairs in the array. See if I can do it with the function for finding anagrams instead
-function procurarStrings(arr){
-    let count = 0;
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = 0; j < arr.length; j++) {
-            if ( (i < j) && (arr[i] === arr[j]) ) {
-                console.log(arr[i] + ' : indexes are ' + i + ' and ' + j);
-                count++;
-            }
-        }
-    }
-    console.log(count);
-}
-procurarStrings(map1);
-
-//Confere se uma string é anagram de outra
-function checkStringsAnagram(a, b) {
-    let len1 = a.length;
-    let len2 = b.length;
-    if(len1 !== len2){
-       console.log('Invalid Input');
+//Verifica se uma string é anagrama de outra
+function verificarAnagrama(a, b) {
+    if(a.length !== b.length){
        return
     }
     let str1 = a.split('').sort().join('');
     let str2 = b.split('').sort().join('');
     if(str1 === str2){
-       console.log("True");
+       return true;
     } else { 
-       console.log("False");
+       return false;
     }
  }
 
- //  checkStringsAnagram("indian","ndiani")
+//  checkStringsAnagram("indian","ndiani")
 // encontreAnagramas('palavra');
 // encontreAnagramas('ovo');
 
-//Procurar anagramas
-function procurarAnagramas(arr){
+//Procurar pares de anagramas
+function procurarPares(palavra){
+    const arrSubstrings = encontrarSubstrings(palavra);
     let count = 0;
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = 0; j < arr.length; j++) {
-            if ( (i < j) && (checkStringAnagram(arr[i], arr[j])) ) {
-                console.log(arr[i] + ' : indexes are ' + i + ' and ' + j);
+    for (let i = 0; i < arrSubstrings.length; i++) {
+        for (let j = 0; j < arrSubstrings.length; j++) {
+            if ( (i < j) && (verificarAnagrama(arrSubstrings[i], arrSubstrings[j])) ) {
+                // console.log(arrSubstrings[i] + ' : indexes are ' + i + ' and ' + j);
                 count++;
             }
         }
     }
-    console.log(count);
+    // console.log(count);
+    return count;
 }
 
+procurarPares('baba');
 
-//Passos:
-//1. Criar array com todas as substrings (done)
-//2. Crirar função que confere se uma string e anagrama de outra
-//3. Usar um nested loop pra iterar cada string do array e ver se é anagrama de outra, usando a função acima como callback e o array como argumento. Cada vez que for, a contagem de anagram pairs aumenta em um. Imprimir a contagem de anagram pairs. 
+const count = procurarPares('baba');
+console.log(count);
 
 
 
